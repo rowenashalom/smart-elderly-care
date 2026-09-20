000072e75f97019a13ea77d6372e35a124f0ff60
Rowena Shalom <rowenashalom@gmail.com>
2026-09-20 14:58:18 +0530

    Prepare backend for Render

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,9 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("MongoDB connected successfully");
 
-        app.listen(PORT, () => {
-            console.log(`Server running on http://localhost:${PORT}`);
-        });
+    app.listen(PORT, "0.0.0.0", () => {
+        console.log(`Server running on port ${PORT}`);
+      });
     })
     .catch((error) => {
         console.log("MongoDB connection error:", error.message);
